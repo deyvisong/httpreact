09e5c7c1a3a30922adc9402315330b05be270ec1
cd ~/code/$deyvisong/dotfiles && zsh git_setup.sh <deyvisonalves@protonmail.com>
2025-01-02 15:45:16 +0100

    Request HTTP

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import { useState, useEffect } from "react";
+
+function App() {
+  const [products, setProducts] = useState([]);
+
+  // 1 - retrive data
+  return (
+    <div className="App">
+      <h1>Product List</h1>
+    </div>
+  );
+}
+
+export default App;
